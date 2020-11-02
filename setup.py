90bad813964fdbb752f7da19bac0b1b961e58ad6
saartochner <saart@lumigo.io>
2020-11-02 17:36:09 +0200

    add initial logs extension infrastructure

--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@ import os
 
 import setuptools
 
-VERSION_PATH = os.path.join(os.path.dirname(__file__), "src", "lumigo_tracer", "VERSION")
-
 setuptools.setup(
     name="lambda_log_shipper",
     version='0.1',
@@ -11,7 +9,7 @@ setuptools.setup(
     author_email="support@lumigo.io",
     description="Ship logs from AWS lambdas with ease",
     long_description_content_type="text/markdown",
-    url="https://github.com/lumigo-io/python_tracer.git",
+    url="https://github.com/lumigo-io/lambda-log-shipper.git",
     package_dir={"": "src"},
     packages=setuptools.find_packages("src"),
     install_requires=["pytest"],
